ed56aa621a11a1aa6d09f7ba66f059eaaeaad56a
Nataliia <maksimeyko@ukr.net>
2022-12-19 21:32:40 +0200

    update files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
-import Statistics from './components/Statistics/Statistics';
-import data from './components/Statistics/data.json';
-import FriendList from './components/Friendlist/Friendlist';
-import friends from './components/Friendlist/friends.json';
-import TransactionsHistory from './components/Transactions/TransanctionsHistory';
-import transactions from './components/Transactions/transactions.json';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import FriendList from './Friendlist/Friendlist';
+import friends from './Friendlist/friends.json';
+import TransactionsHistory from './Transactions/TransanctionsHistory';
+import transactions from './Transactions/transactions.json';
 
 const App = () => {
   return (
