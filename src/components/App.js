7ce7c80a6707e5ce62a3e649e7a4a22ea7d7e6b1
Nataliia <maksimeyko@ukr.net>
2022-12-21 17:16:36 +0200

    update props

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,7 @@ const App = () => {
         likes={user.stats.likes}
       />
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       <FriendList friends={friends} />
       <TransactionsHistory items={transactions} />
     </>
